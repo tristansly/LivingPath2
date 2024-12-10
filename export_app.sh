6875cb2f96530151bf170ddebd513f480d3cd339
ivangrozny <ivan_en_kilt@hotmail.fr>
2024-12-10 18:27:22 +0100

    export mac

--- export_app.sh
+++ export_app.sh
@@ -4,6 +4,7 @@
 
 # pyinstaller -n "app" --onefile --windowed --clean --icon='files/logo.ico' --add-data="files;files" --add-data="plugins;plugins" --additional-hooks-dir=. --additional-hooks-dir="./plugins" -p ".;./plugins"  --collect-submodules="plugins" ./main.py
 
-pyinstaller -n "LivingPath" --onefile --windowed --clean --icon='files/logo.ico' --add-data="files;files" --additional-hooks-dir="hooks" --collect-all "hyperglot" -p "."   ./main.py
+pyinstaller -n "LivingPath" --onefile --windowed --clean --icon='files/logo.ico' --add-data="files:files" --additional-hooks-dir="hooks" --collect-all "hyperglot" -p "."   ./main.py
+
 
 # read -rn1 # keep consol open
